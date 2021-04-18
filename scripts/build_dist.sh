bb8830ac0698463099f8798c92f89b619bf9c352
Shane Liesegang <shane@techie.net>
2021-04-18 15:04:49 +0300

    minify babylon files during build

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+shopt -s globstar
 
 cd "$(dirname "$0")"
 cd ..
@@ -12,5 +13,10 @@ npm run build
 
 cp README.md $ROOT_DIR/dist
 cp LICENSE $ROOT_DIR/dist
+
+for bab in $ROOT_DIR/dist/**/*.babylon; do
+  inp=$(cat $bab)
+  echo $inp | jq -c . > $bab
+done
 
 node ./scripts/generateAssetList.js
